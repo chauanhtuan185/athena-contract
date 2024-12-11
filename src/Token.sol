9c6158c18eb69a805a7783534d26698bf049df43
Kay (Andrew) <chauanhtuan185@gmail.com>
2024-12-11 19:20:34 +0700

    hotfix: remove unnecessary

--- src/Token.sol
+++ src/Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.28;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 //import "forge-std/console2.sol";
 contract Token is ERC20 {
